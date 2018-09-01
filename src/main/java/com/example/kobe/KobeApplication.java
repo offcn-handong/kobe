da2f69d68e945fdfd86881c758dc177bf54db9b6
offcn <wangyi126hd@126.com>
2018-09-01 18:31:44 +0800

    1

---
+++ src/main/java/com/example/kobe/KobeApplication.java
@@ -0,0 +1,12 @@
+package com.example.kobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KobeApplication.class, args);
+    }
+}
